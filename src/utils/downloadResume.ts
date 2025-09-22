f6d6e592f8e7d1dbd5cd269d89ae6b8ba4dac4b1
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-09-22 15:41:39 -0400

    Resume Update

--- src/utils/downloadResume.ts
+++ src/utils/downloadResume.ts
@@ -1,8 +1,8 @@
 export const downloadResume = () => {
-  const resumePath = '/Resume/Alex-Seisler_AI-Software-Engineer_Resume.pdf';
+  const resumePath = '/Resume/Alex_Seisler_Software_Engineer_Resume.pdf';
   const link = document.createElement('a');
   link.href = resumePath;
-  link.download = 'Alex-Seisler_AI-Software-Engineer_Resume.pdf';
+  link.download = 'Alex_Seisler_Software_Engineer_Resume.pdf';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
